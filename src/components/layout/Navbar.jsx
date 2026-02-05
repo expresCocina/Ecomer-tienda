71768e86b64318ce871ff9dec6458a303710d37f
AMC Market <SALCRISTHI5411@GMAIL.COM>
2026-02-05 11:29:34 -0500

    fix: change mobile search placeholder from 'productos' to 'relojes'

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -269,7 +269,7 @@ export const Navbar = () => {
                         <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
                         <input
                             type="text"
-                            placeholder="Buscar productos..."
+                            placeholder="Buscar relojes..."
                             value={searchQuery}
                             onChange={handleSearchChange}
                             onKeyPress={handleSearchKeyPress}
